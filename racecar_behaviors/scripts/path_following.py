ad5b380c3e86d16ad6187eefa4f73bd7594b9927
what <slimshady>
2020-12-02 17:45:31 -0500

    everything working but the camera

--- racecar_behaviors/scripts/path_following.py
+++ racecar_behaviors/scripts/path_following.py
@@ -25,9 +25,9 @@ class PathFollowing:
         self.odom_sub = rospy.Subscriber('odom', Odometry, self.odom_callback, queue_size=1)
         self.obstacle_sub = rospy.Subscriber('obstacle_detected', Bool, self.obstacle_callback, queue_size=1)
         self.blob_sub = rospy.Subscriber('object_detected', Twist, self.blob_callback, queue_size=1)
-
-        #self.state_timer = rospy.Timer( rospy.Duration( 0.05 ), self.state_machine_callback )
-
+        self.state_timer = rospy.Timer( rospy.Duration( 0.05 ), self.state_machine_callback )
+        
+        # VARIABLES
         self.goal_pos_x = 13.5
         self.end_pos_x = 0
         self.goal_pos_y = 2.1
@@ -36,23 +36,75 @@ class PathFollowing:
         self.dont_look_for_objects_no_more = False
         self.state = State.FOLLOW_PATH #
         self.obstacle_detected = False
+        self.R_obstacleDetected = False
+        self.L_obstacleDetected = False
 
-    def state_machine_callback(self):
-        prin("yo")
+    def scan_callback(self, msg):
+        # Because the lidar is oriented backward on the racecar, 
+        # if we want the middle value of the ranges to be forward:
+        l2 = len(msg.ranges)/2;
+        ranges = msg.ranges[l2:len(msg.ranges)] + msg.ranges[0:l2]
+        
+        # Obstacle front?   l2+l2/8
+        self.R_obstacleDetected = False
+        self.L_obstacleDetected = False
+        for i in range(3*l2/4 - l2/16, 3*l2/4 + l2/16) :
+            if np.isfinite(ranges[i]) and ranges[i]>0 and ranges[i] < self.distance:
+                self.R_obstacleDetected = True
+                break
+        for i in range(5*l2/4 - l2/16, 5*l2/4 + l2/16) :
+            if np.isfinite(ranges[i]) and ranges[i]>0 and ranges[i] < self.distance:
+                self.L_obstacleDetected = True
+                break
+
+        self.state_machine_callback( None )
+        
+    def odom_callback(self, msg):
+        self.pos_x = msg.pose.pose.position.x
+        self.pos_y = msg.pose.pose.position.y
+        self.theta = msg.pose.pose.orientation.z
+        # print("orientation", self.theta)
+        if abs(self.pos_x - self.goal_pos_x) < self.delta_pos : # and abs(self.pos_y - self.goal_pos_y) < self.delta_pos)
+            if self.state == State.FOLLOW_PATH:
+                if abs(self.theta) < 0.5:
+                    print("DOING A 180")
+                    self.state = State.DID_A_FULL_ONE_EIGHTY
+
+    def blob_callback(self, msg):
+        # print('Distance' , msg.linear.x)
+        # print('Angle', msg.angular.z)
+        self.angle_blob = msg.angular.z
+        if(not self.dont_look_for_objects_no_more):
+            if (msg.linear.x) < 2.0:
+                self.state = State.WAIT_FOR_SCAN
+                print('state: Waiting for scan')
+
+            elif (msg.linear.x) < 2.5:
+                self.state = State.CENTER_ON_OBJECT
+                print('state: Trying to Center the object!')
+
+    def obstacle_callback(self, msg):
+        self.obstacle_detected = msg.data
+        if self.obstacle_detected:
+            print("Obstacle :", msg.data)
+
+    def state_machine_callback(self, state_timer):
         if self.state == State.CENTER_ON_OBJECT:
-            mode_center_on_object()
+            self.mode_center_on_object()
 
         elif self.state == State.WAIT_FOR_SCAN:
-            mode_wait_for_scan()
+            self.mode_wait_for_scan()
 
         elif self.state == State.FOLLOW_PATH:
-            mode_follow_path()
+            self.mode_follow_path()
 
         elif self.state == State.DID_A_FULL_ONE_EIGHTY:
-            mode_180_turn()
+            self.mode_180_turn()
 
         elif self.state == State.REWIND:
-            rewind()
+            self.rewind()
+
+        self.cmd_vel_pub.publish(self.twist)
 
     def mode_center_on_object(self):
         self.twist.linear.x = 0.2
@@ -87,66 +139,6 @@ class PathFollowing:
         if abs(self.theta) > 0.9:
             print("state: Follow Path")
             self.state = State.FOLLOW_PATH
-
-    def scan_callback(self, msg):
-        # Because the lidar is oriented backward on the racecar, 
-        # if we want the middle value of the ranges to be forward:
-        l2 = len(msg.ranges)/2;
-        ranges = msg.ranges[l2:len(msg.ranges)] + msg.ranges[0:l2]
-        
-        # Obstacle front?   l2+l2/8
-        R_obstacleDetected = False
-        L_obstacleDetected = False
-        for i in range(3*l2/4 - l2/16, 3*l2/4 + l2/16) :
-            if np.isfinite(ranges[i]) and ranges[i]>0 and ranges[i] < self.distance:
-                R_obstacleDetected = True
-                print("obst detected right")
-                break
-        for i in range(5*l2/4 - l2/16, 5*l2/4 + l2/16) :
-            if np.isfinite(ranges[i]) and ranges[i]>0 and ranges[i] < self.distance:
-                L_obstacleDetected = True
-                print("obst detected left")
-                break
-
-        if(R_obstacleDetected and not L_obstacleDetected):
-            self.twist.angular.z = self.max_steering
-        elif(L_obstacleDetected and not R_obstacleDetected):
-            self.twist.angular.z = -(self.max_steering)
-        else:
-            self.twist.angular.z = 0
-
-        self.twist.linear.x = self.max_speed           
-        self.cmd_vel_pub.publish(self.twist)
-        
-    def odom_callback(self, msg):
-        self.pos_x = msg.pose.pose.position.x
-        self.pos_y = msg.pose.pose.position.y
-        self.theta = msg.pose.pose.orientation.z
-        # print("orientation", self.theta)
-        if abs(self.pos_x - self.goal_pos_x) < self.delta_pos : # and abs(self.pos_y - self.goal_pos_y) < self.delta_pos)
-            if self.state == State.FOLLOW_PATH:
-                if abs(self.theta) < 0.5:
-                    print("DOING A 180")
-                    self.state = State.DID_A_FULL_ONE_EIGHTY
-
-    def blob_callback(self, msg):
-        # print('Distance' , msg.linear.x)
-        # print('Angle', msg.angular.z)
-        self.angle_blob = msg.angular.z
-        if(not self.dont_look_for_objects_no_more):
-            if (msg.linear.x) < 2.0:
-                self.state = State.WAIT_FOR_SCAN
-                print('state: Waiting for scan')
-
-            elif (msg.linear.x) < 2.5:
-                self.state = State.CENTER_ON_OBJECT
-                print('state: Trying to Center the object!')
-
-    def obstacle_callback(self, msg):
-        self.obstacle_detected = msg.data
-        if self.obstacle_detected:
-            print("Obstacle :", msg.data)
-
 
 def main():
     rospy.init_node('path_following')
