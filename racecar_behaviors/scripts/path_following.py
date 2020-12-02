6a7839a58bf325dccd162525703dc5d7a213b941
what <slimshady>
2020-12-02 17:19:08 -0500

    fixed it, but still using old logic

--- racecar_behaviors/scripts/path_following.py
+++ racecar_behaviors/scripts/path_following.py
@@ -17,7 +17,6 @@ class State:
 
 class PathFollowing:
     def __init__(self):
-        print("path_following_init")
         self.twist = Twist()
         self.max_speed = rospy.get_param('~max_speed', 0.25)
         self.max_steering = rospy.get_param('~max_steering', 0.37)
@@ -25,80 +24,99 @@ class PathFollowing:
         self.scan_sub = rospy.Subscriber('scan', LaserScan, self.scan_callback, queue_size=1)
         self.odom_sub = rospy.Subscriber('odom', Odometry, self.odom_callback, queue_size=1)
         self.obstacle_sub = rospy.Subscriber('obstacle_detected', Bool, self.obstacle_callback, queue_size=1)
-        self.state_timer = rospy.Timer( rospy.Duration( 0.05 ), self.state_machine_cb )
+        self.blob_sub = rospy.Subscriber('object_detected', Twist, self.blob_callback, queue_size=1)
+
+        #self.state_timer = rospy.Timer( rospy.Duration( 0.05 ), self.state_machine_callback )
 
         self.goal_pos_x = 13.5
         self.end_pos_x = 0
         self.goal_pos_y = 2.1
         self.delta_pos = 0.5
-        self.blob_sub = rospy.Subscriber('object_detected', Twist, self.blob_callback, queue_size=1)
         self.distance = 1.5
         self.dont_look_for_objects_no_more = False
         self.state = State.FOLLOW_PATH #
         self.obstacle_detected = False
 
-
-    def state_machine_cb:
+    def state_machine_callback(self):
         prin("yo")
         if self.state == State.CENTER_ON_OBJECT:
-            self.twist.linear.x = 0.2
-            self.twist.angular.z = self.angle_blob
+            mode_center_on_object()
 
         elif self.state == State.WAIT_FOR_SCAN:
-            self.twist.linear.x = 0
-            self.twist.angular.z = 0
-            rospy.sleep(5)
-            self.state = State.FOLLOW_PATH
-            # self.dont_look_for_objects_no_more = True
+            mode_wait_for_scan()
 
         elif self.state == State.FOLLOW_PATH:
-            if(self.R_obstacleDetected and not self.L_obstacleDetected):
-                self.twist.angular.z = self.max_steering
-            elif(self.L_obstacleDetected and not self.R_obstacleDetected):
-                self.twist.angular.z = -(self.max_steering)
-            else:
-                self.twist.angular.z = 0
-            self.twist.linear.x = self.max_speed
+            mode_follow_path()
 
         elif self.state == State.DID_A_FULL_ONE_EIGHTY:
-            self.twist.linear.x = 0.2
-            self.twist.angular.z = self.max_steering
-            if self.obstacle_detected:
-                print("state: Rewinding")
-                self.state = State.REWIND
+            mode_180_turn()
 
         elif self.state == State.REWIND:
-            self.twist.linear.x = -0.2
-            self.twist.angular.z = -self.max_steering
-            if abs(self.theta) > 0.9:
-                print("state: Follow Path")
-                self.state = State.FOLLOW_PATH
+            rewind()
 
-        self.cmd_vel_pub.publish(self.twist)
+    def mode_center_on_object(self):
+        self.twist.linear.x = 0.2
+        self.twist.angular.z = self.angle_blob
 
+    def mode_wait_for_scan(self):
+        self.twist.linear.x = 0
+        self.twist.angular.z = 0
+        rospy.sleep(5)
+        self.state = State.FOLLOW_PATH
+        # self.dont_look_for_objects_no_more = True
+
+    def mode_follow_path(self):
+        if(self.R_obstacleDetected and not self.L_obstacleDetected):
+            self.twist.angular.z = self.max_steering
+        elif(self.L_obstacleDetected and not self.R_obstacleDetected):
+            self.twist.angular.z = -(self.max_steering)
+        else:
+            self.twist.angular.z = 0
+        self.twist.linear.x = self.max_speed
+
+    def mode_180_turn(self):
+        self.twist.linear.x = 0.2
+        self.twist.angular.z = self.max_steering
+        if self.obstacle_detected:
+            print("state: Rewinding")
+            self.state = State.REWIND
+
+    def rewind(self):
+        self.twist.linear.x = -0.2
+        self.twist.angular.z = -self.max_steering
+        if abs(self.theta) > 0.9:
+            print("state: Follow Path")
+            self.state = State.FOLLOW_PATH
 
     def scan_callback(self, msg):
-        print("scan_callback")
         # Because the lidar is oriented backward on the racecar, 
         # if we want the middle value of the ranges to be forward:
-        l2 = len(msg.ranges)/2
+        l2 = len(msg.ranges)/2;
         ranges = msg.ranges[l2:len(msg.ranges)] + msg.ranges[0:l2]
         
         # Obstacle front?   l2+l2/8
-        self.R_obstacleDetected = False
-        self.L_obstacleDetected = False
+        R_obstacleDetected = False
+        L_obstacleDetected = False
         for i in range(3*l2/4 - l2/16, 3*l2/4 + l2/16) :
             if np.isfinite(ranges[i]) and ranges[i]>0 and ranges[i] < self.distance:
-                self.R_obstacleDetected = True
-                #print("obst detected right")
+                R_obstacleDetected = True
+                print("obst detected right")
                 break
         for i in range(5*l2/4 - l2/16, 5*l2/4 + l2/16) :
             if np.isfinite(ranges[i]) and ranges[i]>0 and ranges[i] < self.distance:
-                self.L_obstacleDetected = True
-                #print("obst detected left")
+                L_obstacleDetected = True
+                print("obst detected left")
                 break
 
-        
+        if(R_obstacleDetected and not L_obstacleDetected):
+            self.twist.angular.z = self.max_steering
+        elif(L_obstacleDetected and not R_obstacleDetected):
+            self.twist.angular.z = -(self.max_steering)
+        else:
+            self.twist.angular.z = 0
+
+        self.twist.linear.x = self.max_speed           
+        self.cmd_vel_pub.publish(self.twist)
         
     def odom_callback(self, msg):
         self.pos_x = msg.pose.pose.position.x
@@ -110,12 +128,6 @@ class PathFollowing:
                 if abs(self.theta) < 0.5:
                     print("DOING A 180")
                     self.state = State.DID_A_FULL_ONE_EIGHTY
-
-        # if abs(self.pos_x - self.end_pos_x) < self.delta_pos : # and abs(self.pos_y - self.goal_pos_y) < self.delta_pos)
-        #     if self.state == State.FOLLOW_PATH:
-        #         if abs(self.theta) > 0.5:
-        #         self.twist.linear.x = 0
-        #         self.cmd_vel_pub.publish(self.twist)
 
     def blob_callback(self, msg):
         # print('Distance' , msg.linear.x)
